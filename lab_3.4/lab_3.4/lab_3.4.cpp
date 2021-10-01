649c32c9e8d9bcf9002513b8ad48a34ac70124bb
ArtemSadovenko <90398799+ArtemSadovenko@users.noreply.github.com>
2021-10-01 09:36:14 +0300

    Add files via upload

---
+++ lab_3.4/lab_3.4/lab_3.4.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	double x;
+	double y;
+	double r;
+
+	cout << "x = "; cin >> x;
+	cout << "r = "; cin >> r;
+	cout << "y = "; cin >> y;
+
+	if ((y >= x && y <= sqrt(r*r - pow((x+r),2)) + r) || ((y <= x && y >= sqrt(r * r - pow((x - r), 2) - r)))) {
+		cout << "yes";
+	}
+	else {
+		cout << "no";
+	}
+
+	return 0;
+}
